d1b7308ceeef6b7b7e58cd870b2a97c28b8b2748
chenshao <cshao676@gmail.com>
2025-03-05 22:24:35 +0100

    upload tune script

--- GRAND_LP_chen/README.md
+++ GRAND_LP_chen/README.md
@@ -90,7 +90,7 @@ python main_grand.py  --dataset Cora --device 0 --no_early --beltrami
 
 CUDA_LAUNCH_BLOCKING=1 python allin_grand_ncnc.py --data_name ogbl-collab --device 0  --beltrami --predictor cn1
 
-python allin_grand_ncnc.py   --xdp 0.25 --tdp 0.05 --pt 0.1 --gnnedp 0.25 --preedp 0.0 --predp 0.3 --gnndp 0.1  --probscale 2.5 --proboffset 6.0 --alpha 1.05  --gnnlr 0.0082 --prelr 0.0037  --batch_size 65536  --ln --lnnn --predictor cn1 --dataset collab  --epochs 100 --runs 2 --hiddim 64 --mplayers 1  --testbs 131072  --maskinput --use_valedges_as_input   --res  --use_xlin  --tailact 
+python allin_grand_ncnc.py   --xdp 0.25 --tdp 0.05 --pt 0.1 --gnnedp 0.25 --preedp 0.0 --predp 0.3 --gnndp 0.1  --probscale 2.5 --proboffset 6.0 --alpha 1.05  --gnnlr 0.0082 --prelr 0.0037  --batch_size 65536  --ln --lnnn --predictor cn1 --dataset collab  --epochs 100 --runs 2 --hidden_dim 128 --mplayers 1  --testbs 131072  --maskinput --use_valedges_as_input   --res  --use_xlin  --tailact 
 
 
 with this mail I would like to submit my code to you:
--- GRAND_LP_chen/grand_ogb.sh
+++ GRAND_LP_chen/grand_ogb.sh
@@ -18,7 +18,7 @@
 # Request GPU resources
 #SBATCH --gres=gpu:1
 
-source /hkfs/home/project/hk-project-test-p0021478/cc7738/anaconda3/etc/profile.d/conda.sh
+# source /hkfs/home/project/hk-project-test-p0021478/cc7738/anaconda3/etc/profile.d/conda.sh
 
 conda activate base
 # <<< conda initialize <<<
@@ -44,19 +44,29 @@ echo "Start training grand on $data"
 # python main_grand.py  --dataset ogbl-citation2 --device 0 --no_early --beltrami
 # python main_grand.py  --dataset ogbl-collab --device 0 --no_early --beltrami
 
-python allin_grand.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000
-python allin_grand_original.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000
-python allin_grand_original.py  --data_name ogbl-collab --device 1 --gcn True --epoch 1000
+# python allin_grand.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000
+# python allin_grand_original.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000
+# python allin_grand_original.py  --data_name ogbl-collab --device 1 --gcn True --epoch 1000
 
-to sum up 
+# to sum up 
 
-for grand
-`python allin_grand_original.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000``
+# for grand
+# `python allin_grand_original.py  --data_name ogbl-collab --device 0 --no_early --beltrami --epoch 1000``
 
-for gcn
-`python allin_grand_original.py  --data_name ogbl-collab --device 1 --gcn True --epoch 1000`
+# for gcn
+# `python allin_grand_original.py  --data_name ogbl-collab --device 1 --gcn True --epoch 1000`
 
-for comparison with the same hidden channels
-`
-python gnn_ogb_heart.py --data_name ogbl-collab --gnn_model GCN --hidden_channels 18 --lr 0.001 --dropout 0.0 --num_layers 3 --num_layers_predictor 3 --epochs 800 --kill_cnt 100 --batch_size 16394 --runs 2 --device 2
-`
+# for comparison with the same hidden channels
+# `
+# python gnn_ogb_heart.py --data_name ogbl-collab --gnn_model GCN --hidden_channels 18 --lr 0.001 --dropout 0.0 --num_layers 3 --num_layers_predictor 3 --epochs 800 --kill_cnt 100 --batch_size 16394 --runs 2 --device 2
+# `
+python grand_ncnc_tune.py --data_name ogbl-collab --xdp 0.25 --tdp 0.05 --pt 0.1 \
+                            --gnnedp 0.25  --preedp 0.0 --predp 0.3 --gnndp 0.1   --probscale 2.5 --proboffset 6.0 \
+                            --alpha 1.05  --gnnlr 0.0082 --prelr 0.0037  --batch_size 65536  --ln --lnnn --predictor cn1 \
+                            --epochs 2 --runs 1 --hidden_dim 128 --mplayers 1  --testbs 131072  --maskinput  \
+                            --use_valedges_as_input  --res  --use_xlin  --tailact 
+
+
+#TODO understand better the parameter roles, and the model structure
+#TODO analyse the result of prediction w.r.t correlation, error analyse w.r.t. degree distribution and node feature difference 
+#PRETEST synthetic graph visualization, parameters description and intial result with increasing number of node, edge distribution  
